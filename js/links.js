220040cf533318d11f331f564ce9b159dde38197
Mixo <mixo.salah1@gmail.com>
2025-04-12 09:42:25 +0200

    better management of links

---
+++ js/links.js
@@ -0,0 +1,16 @@
+const externalLinks = {
+    github: "https://github.com/mixomalepfane",
+    codeberg: "https://codeberg.org/mixo",
+    linkedin: "https://www.linkedin.com/in/mixo-malepfane",
+    x: "https://x.com/mixomalepfane",
+    email: "mailto:malepfanemixo@gmail.com",
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (const [key, url] of Object.entries(externalLinks)) {
+    document.querySelectorAll(`[data-link="${key}"]`).forEach(el => {
+      el.setAttribute("href", url);
+    });
+  }
+});
+
