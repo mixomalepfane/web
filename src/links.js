4611225f8eac7488806af9b069932c91477f44ba
Mixo <mixo.salah1@gmail.com>
2025-04-12 11:11:00 +0200

    add education

--- src/links.js
+++ src/links.js
@@ -4,6 +4,7 @@ const externalLinks = {
     linkedin: "https://www.linkedin.com/in/mixo-malepfane",
     x: "https://x.com/mixomalepfane",
     email: "mailto:malepfanemixo@gmail.com",
+    north_west: "https://www.nwu.ac.za",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
